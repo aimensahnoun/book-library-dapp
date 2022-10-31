c11eec4bcaa4a9edb6f67342d632a88f578d8beb
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-10-31 17:40:01 +0300

    Add nav component with rainbowkit

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,17 @@
-import { testAtom } from "@/utils/global-state"
+// Dependencies import
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAtom } from "jotai"
+
+// Utils import
+import { testAtom } from "@/utils/global-state"
 
 const Navbar = () => {
 
     const [test, setTest] = useAtom(testAtom)
 
-    return <nav>
-        {test}
+    return <nav className='flex p-4 items-center justify-between w-full border-b-[1px] border-b-gray-700'>
+        <span className='font-bold text-lg'>Book Library | LimeAcademy</span>
+        <ConnectButton />
     </nav>
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,55 @@
-import '../styles/globals.css'
+// NextJS import
 import type { AppProps } from 'next/app'
+
+// Custom component import
 import Navbar from '@/components/navbar'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Navbar />
+// CSS import
+import '@rainbow-me/rainbowkit/styles.css';
+import '../styles/globals.css'
 
-    <Component {...pageProps} />
-  </>
+// Dependencies import
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Book Library | LimeAcademy',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider theme={darkTheme({
+      accentColor : '#4946FF',
+    })} chains={chains}>
+      <Navbar />
+
+      <Component {...pageProps} />
+    </RainbowKitProvider>
+  </WagmiConfig>
 }
